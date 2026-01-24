5d1d3666fd1f82f92c7e9dc05114b4dbdf92bd5c
Madnur <zhanprasetyaningrat@gmail.com>
2026-01-24 19:06:57 +0700

    proto; update; fix; custom; done add	:   		> BuildClearChat ( appstate ) fix	: 		> SenderAlt empty if IsFromMe

--- retry.go
+++ retry.go
@@ -103,10 +103,6 @@ func (cli *Client) addRecentMessage(ctx context.Context, to types.JID, id types.
 
 func (cli *Client) getRecentMessage(to types.JID, id types.MessageID) RecentMessage {
 	cli.recentMessagesLock.RLock()
-<<<<<<< HEAD
-	defer cli.recentMessagesLock.RUnlock()
-	return cli.recentMessagesMap[recentMessageKey{to, id}]
-=======
 	key := recentMessageKey{to, id}
 	msg, _ := cli.recentMessagesMap[key]
 	if cache.DefaultSentCache != nil && msg.wa == nil {
@@ -120,7 +116,6 @@ func (cli *Client) getRecentMessage(to types.JID, id types.MessageID) RecentMess
 	}
 	cli.recentMessagesLock.RUnlock()
 	return msg
->>>>>>> 7fe031b (feat: android)
 }
 
 func (cli *Client) getMessageForRetry(ctx context.Context, receipt *events.Receipt, messageID types.MessageID) (*RecentMessage, error) {
